1694cbf143c4aa80fc89e3226181ed8772c7da52
João Barbosa <contato.jsbarbosa@gmail.com>
2023-06-19 12:08:37 -0300

    Estruturação HTML

---
+++ README.mk
@@ -0,0 +1,64 @@
+## Conversor de Unidades de Medidas♻️📐
+
+Este é um projeto simples de um conversor de unidades de medidas desenvolvido com HTML, CSS e JavaScript. Ele permite que o usuário converta valores de uma medida para outra, selecionando as unidades de origem e destino.
+
+Este projeto foi desenvolvido através do curso "[Projetos de Javascript](https://www.udemy.com/share/108Qsq3@obxmxIYkcaN7YZ88M2ek2CJHI2R0iWqH7fdQqTGp9l24tOpukT91SeY8f32XEhxumA==/)" do professor Matheus Battisti.
+
+### Funcionalidades
+
+- Conversão de diferentes unidades de medida, como comprimento, temperatura, peso, etc.
+- Interface amigável e fácil de usar.
+- Resultado da conversão exibido em uma div.
+
+### Pré-requisitos
+
+Antes de executar o projeto, verifique se você possui os seguintes requisitos:
+
+- Navegador web moderno que suporte HTML5, CSS3 e JavaScript.
+
+### Executando o projeto
+
+Para executar o projeto, siga as etapas abaixo:
+
+1. Clone ou faça o download do repositório para o seu computador.
+
+2. Abra o arquivo ***index.html*** em um navegador web.
+
+### Como usar
+
+1. Preencha o valor da medida no campo de entrada fornecido.
+2. Selecione a unidade de medida de origem no menu suspenso.
+3. Selecione a unidade de medida de destino no segundo menu suspenso.
+4. O resultado da conversão será exibido automaticamente na div de resultado.
+
+### Personalizando o projeto
+
+Se desejar personalizar o projeto, você pode fazer as seguintes alterações:
+
+1. Alterar o estilo da página editando o arquivo ***styles.css**.*
+
+2. Adicionar ou remover unidades de medida no arquivo ***script.js**.*
+3. Modificar a estrutura HTML no arquivo index.html para atender às suas necessidades.
+
+### Contribuindo
+
+Contribuições são bem-vindas! Se você quiser contribuir com este projeto, siga as etapas abaixo:
+
+1. Faça um fork deste repositório.
+
+2. Crie uma branch para sua feature (git checkout -b feature/sua-feature).
+3. Faça commit das suas alterações (***git commit -am*** 'Adicionando uma nova feature').
+4. Envie a branch (***git push origin feature/sua-feature***).
+5. Abra um Pull Request.
+
+### Contato
+
+Se você tiver alguma dúvida ou sugestão, sinta-se à vontade para entrar em contato.
+
+Nome: João Barbosa
+
+Portfolio: [https://joaosbarbosa.com.br/](https://joaosbarbosa.com.br/)
+
+LinkedIn: [João Barbosa](https://www.linkedin.com/in/joao-da-silva-barbosa)
+
+Projeto: https://github.com/JoaoSBarbosa/unit-converter
